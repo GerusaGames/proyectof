651cb7b87d494702b4a72341df750e7120b1c62a
GerusaGames <36251466+GerusaGames@users.noreply.github.com>
2018-07-11 08:38:35 -0500

    BACKEND UPDATE
    incluye la clase handler, que serializa y deserializa el contenido. la super clase priducto ha sido ligeramente modificada. y las subclases ahora tienen metodos distintos

---
+++ ProyectoCatalago/src/cd.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class cd extends producto implements Serializable{
+	public String autor;
+	public String interprete;
+	public int numeroCanciones;
+	public String genero;
+	private ArrayList<String> canciones = new ArrayList<String>();
+
+	public cd(String a, String c, int nc , String g){
+		this.autor = a;
+		this.interprete = c;
+		this.numeroCanciones = nc;
+		this.genero = g;
+	}
+	public cd (String nom , String des ,int sk, int co, String a, String c, int nc , String g){
+		super(nom,des,sk,co);
+		this.autor = a;
+		this.interprete = c;
+		this.numeroCanciones = nc;
+		this.genero = g;
+	}
+	
+	@Override
+	public void mostrarDetalles(){
+		System.out.println("----------");
+		System.out.println("PRODUCTO: CD");
+		System.out.println("----------");
+
+		super.mostrarDetalles();
+		System.out.println("");
+		System.out.println("Autor: "+ this.autor);
+		System.out.println("interprete: "+ this.interprete);
+		System.out.println("Numero de Canciones: "+ this.numeroCanciones);
+		System.out.println("Genero: "+ this.genero);
+	}
+
+	@Override
+	public String obtenerInfoString(){
+		return super.obtenerInfoString() + this.autor + "\n" + this.interprete + "\n" + this.numeroCanciones + "\n" + this.genero + "\n";
+	}
+}
--- ProyectoCatalago/src/producto.java
+++ ProyectoCatalago/src/producto.java
@@ -1,10 +1,17 @@
-public class producto{
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+class producto implements Serializable{
 	private String nombre;
 	private int sku;
 	private int costo;
 	private String descripcion;
 	public producto(){}
-        
 	public void mostrarDetalles(){
 		System.out.println("Nombre: " + nombre);
 		System.out.println("Sku: "+ sku);
@@ -12,10 +19,10 @@ public class producto{
 		System.out.println("Descricpion: " + descripcion);
 	}
 	public producto(String nom,String des , int sk, int co){
-		this.nombre = nom;
-		this.sku = sk;
-		this.costo = co;
-		this.descripcion = des;
+		nombre = nom;
+		sku = sk;
+		costo = co;
+		descripcion = des;
 	}
 	public void cambiarNombre(String t){
 		this.nombre = t;
@@ -26,10 +33,16 @@ public class producto{
 	public String obtenerNombre(){
 		return this.nombre;
 	}
+	public int obtenerSku(){
+		return this.sku;
+	}
 	public int obtenerCosto(){
 		return this.costo;
 	}
 	public String obtenerDescripcion(){
 		return this.descripcion;
+	}
+	public String obtenerInfoString(){
+		return this.nombre + "\n" + this.sku + "\n" + this.costo + "\n" + this.descripcion + "\n";
 	}
 }
